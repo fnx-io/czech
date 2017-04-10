19b6c6e9d53035b7a14ca4781e8b8544ab07bc25
Tomucha <tomucha@gmail.com>
2017-04-10 10:10:55 +0200

    Docs.

--- lib/src/czech_personal_id.dart
+++ lib/src/czech_personal_id.dart
@@ -1,6 +1,10 @@
 // Copyright (c) 2017, tomucha. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+///
+/// Is `value` valid Czech national identification number?
+/// Value should contain a slash (/) followed by a identification number suffix.
+///
 bool isCzechIdNumber(String value) {
   // TODO
   return false;
