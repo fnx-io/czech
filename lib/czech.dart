08ed05ec60c86f7ab687e8b7195a1c9e5779e586
Tomucha <tomucha@gmail.com>
2017-04-10 10:03:52 +0200

    Skeleton.

---
+++ lib/czech.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2017, tomucha. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library czech;
+
+export 'src/czech_base.dart';
+export 'src/czech_bank_account.dart';
+export 'src/czech_personal_id.dart';
---
+++ lib/src/czech_bank_account.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2017, tomucha. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+///
+/// Is `value` a corrent bank account number?
+///
+/// `withBankCode` expects slash and bank code at the end of the account number.
+///
+bool isCzechBankAccount(String value, {withBankCode: true}) {
+  return false;
+}
