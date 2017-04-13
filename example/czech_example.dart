0faf77bac83c6077c1e224fb4a1cda891d97a4f3
Tomucha <tomucha@gmail.com>
2017-04-13 17:42:15 +0200

    README finetuning.

--- example/czech_example.dart
+++ example/czech_example.dart
@@ -4,16 +4,25 @@
 import 'package:czech/czech.dart' as cz;
 
 main() {
+
   // a.k.a Rodné číslo
   if (cz.isCzechPersonalIdNumber("780324/6543")) {
     print("Valid!");
   }
+
   // a.k.a. IČ
   if (cz.isCzechCompanyIdNumber("04626133")) {
     print("Valid!");
   }
-  // a.k.a. Číslo účtu
+
+  // a.k.a. Číslo účtu - with bank code by default
   if (cz.isCzechBankAccount("3033/2700")) {
     print("Valid!");
   }
+
+  // a.k.a. Číslo účtu - without a bank code
+  if (cz.isCzechBankAccount("3033", withBankCode: false)) {
+    print("Valid!");
+  }
+
 }
